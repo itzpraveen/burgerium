39c6de205105a2f1b36fa685d3efc04e5631e196
itzpraveen <itzpraveen@users.noreply.github.com>
2025-12-09 02:38:29 +0530

    ci: add github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Update if repo/owner changes
+  site: 'https://itzpraveen.github.io/burgerium',
+});
